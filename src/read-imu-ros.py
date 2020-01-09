b0f885a1c6fa536de30408675a1755879b2deb10
Devansh Batra <devanshbatra04@gmail.com>
2020-01-09 18:07:28 +0530

    published IMU message

--- src/read-imu-ros.py
+++ src/read-imu-ros.py
@@ -7,19 +7,62 @@ from sensor_msgs.msg import Imu
 
 imu_port = '/dev/ttyACM0'
 ard = serial.Serial(imu_port,57600,timeout=5)
-flag = True
 
-while True:
+# while True:
+
+
+
+def talker():
+    pub = rospy.Publisher('imu', Imu, queue_size=10)
+    rospy.init_node('ros_erle_imu', anonymous=True)
+    rate = rospy.Rate(10)
+
+    msg = Imu()
+    flag = True
+    while not rospy.is_shutdown():
+        try:
+            if flag:
+                flag = False
+                b = ard.readline()
+                # time.sleep(3)
+            b = ard.readline()  # read a byte string
+
+            string_n = b.decode()  # decode byte string into Unicode
+            string = string_n.rstrip()  # remove \n and \r
+            imu_obj = json.loads(string)
+            print(imu_obj)
+
+
+        # Fill message
+            msg.orientation.x = imu_obj["orientationx"]
+            msg.orientation.y = imu_obj["orientationy"]
+            msg.orientation.z = imu_obj["orientationz"]
+            msg.orientation.w = imu_obj["orientationw"]
+            msg.orientation_covariance[0] = imu_obj["orientationx"] * imu_obj["orientationx"]
+            msg.orientation_covariance[0] = imu_obj["orientationy"] * imu_obj["orientationy"]
+            msg.orientation_covariance[0] = imu_obj["orientationz"] * imu_obj["orientationz"]
+            msg.angular_velocity.x = imu_obj["angular_velocityx"]
+            msg.angular_velocity.y = imu_obj["angular_velocityy"]
+            msg.angular_velocity.z = imu_obj["angular_velocityz"]
+            msg.angular_velocity_covariance[0] = imu_obj["angular_velocityx"] * imu_obj["angular_velocityx"]
+            msg.angular_velocity_covariance[4] = imu_obj["angular_velocityy"] * imu_obj["angular_velocityy"]
+            msg.angular_velocity_covariance[8] = imu_obj["angular_velocityz"] * imu_obj["angular_velocityz"]
+
+            msg.linear_acceleration.x = imu_obj["linear_accelerationx"]
+            msg.linear_acceleration.y = imu_obj["linear_accelerationy"]
+            msg.linear_acceleration.z = imu_obj["linear_accelerationz"]
+            msg.linear_acceleration_covariance[0] = imu_obj["linear_accelerationx"] * imu_obj["linear_accelerationx"]
+            msg.linear_acceleration_covariance[4] = imu_obj["linear_accelerationy"] * imu_obj["linear_accelerationy"]
+            msg.linear_acceleration_covariance[8] = imu_obj["linear_accelerationz"] * imu_obj["linear_accelerationz"]
+
+            pub.publish(msg)
+
+            rate.sleep()
+        except (json.decoder.JSONDecodeError, UnicodeDecodeError):
+            continue
+
+if __name__ == '__main__':
     try:
-        if flag:
-            flag = False
-            b = ard.readline()
-            # time.sleep(3)
-        b = ard.readline()  # read a byte string
-
-        string_n = b.decode()  # decode byte string into Unicode
-        string = string_n.rstrip()  # remove \n and \r
-        imu_obj = json.loads(string)
-        print(imu_obj)
-    except (json.decoder.JSONDecodeError, UnicodeDecodeError):
-        continue
+        talker()
+    except rospy.ROSInterruptException:
+        pass
