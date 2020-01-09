8ed0e5ddb6b132be90e2b514c0e621476d9104d7
Devansh Batra <devanshbatra04@gmail.com>
2020-01-09 17:27:48 +0530

    read imu message

--- src/read-imu.py
+++ src/read-imu.py
@@ -1,27 +1,20 @@
-import asyncio
-import websockets
 import serial
 import syslog
 import time
+import json
 
 imu_port = '/dev/ttyACM0'
-ard = serial.Serial(port,57600,timeout=5)
+ard = serial.Serial(imu_port,57600,timeout=5)
+flag = True
 
-import asyncio
-import websockets
+while True:
+    if flag:
+        flag = False
+        b = ard.readline()
+        time.sleep(1)
+    b = ard.readline()  # read a byte string
 
-def echo(websocket, path):
-    while True:
-        # print("here")
-        b = ard.readline()  # read a byte string
-        string_n = b.decode()  # decode byte string into Unicode
-        string = string_n.rstrip()  # remove \n and \r
-        # readings = string.split(",")
-        # if len(readings[0]) != 19:
-        #     continue
-        # print(readings)
-        # await websocket.send(readings[0])
-
-asyncio.get_event_loop().run_until_complete(
-websockets.serve(echo, '0.0.0.0', 8765))
-asyncio.get_event_loop().run_forever()
+    string_n = b.decode()  # decode byte string into Unicode
+    string = string_n.rstrip()  # remove \n and \r
+    imu_obj = json.loads(string)
+    print(imu_obj)
